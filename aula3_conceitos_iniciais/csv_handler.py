c4703bb230d8e53db2c98bbdb5c35fb9d18d4121
NatashaB-randao <natashabrandao57@hotmail.com>
2025-03-10 09:31:50 -0300

    Construindo a classe

---
+++ aula3_conceitos_iniciais/csv_handler.py
@@ -0,0 +1,9 @@
+class CsvHandler:
+    def __init__(self, directory) -> None:
+        self.dir = directory
+
+    def insert_data_in_csv(self, data):
+        print(f"Inserindo {data} em {self.dir}")
+
+    def read_data(self):
+        print(f"Lendo dados de {self.dir}")
